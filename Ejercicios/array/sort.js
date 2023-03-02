4f97b7fd377fb964ef959d12d9bc0003082d7579
nol4lej <nome.ahumada@outlook.com>
2023-03-01 22:10:04 -0300

    Avance de ejercicio PRODUCO+IVA;  variables/input/elementos/atributos/metodos

--- Ejercicios/array/sort.js
+++ Ejercicios/array/sort.js
@@ -1,39 +1,12 @@
-// let dias = ["lunes", "martes", "miercoles"]
-
-// dias.forEach(function(elemento, index, array){
-//     array[index] = elemento.toUpperCase()
-// });
-
-// console.table(dias)
-// _______________
-// const array = [1,2,3,4,5,6,7,8,9,10];
-// let suma = 0;
-// let suma2 = 0;
-
-// const pares = array.filter(elemento => elemento % 2 == 0)
-// pares.forEach(element => suma = element + suma)
-// console.log(suma)
-
-// const impares = array.filter(elemento => elemento % 2 != 0)
-// impares.forEach(element => suma2 = element + suma2)
-// console.log(suma2)
-// _______________
-
 // el método sort() ordena los elementos de un arreglo localmente y devuelve el arreglo ordenado. 
-
 
 const lista = ["Juan 5","Luisa 7","Ana 4","Pedro 3"];
 
 const listaNumeros = [5,6,1,7,3,9,4,3];
 console.log(listaNumeros)
-console.log(listaNumerossd)
+
 
 // listaNumeros.sort( (a,b) => a - b)
-
-// function comparar (listaNumeros){
-//     if 
-// }
-
 
 
 // console.log(listaNumeros)
