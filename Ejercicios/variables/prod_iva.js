8fad403998114f54d43ed3741ddeca8a4db94a5a
nol4lej <nome.ahumada@outlook.com>
2023-03-03 11:34:18 -0300

    Añadido condicion si scriptHabilitado

--- Ejercicios/variables/prod_iva.js
+++ Ejercicios/variables/prod_iva.js
@@ -8,61 +8,68 @@
 
 // se captura el contenedor donde se integrarán los datos
 const div = document.getElementById("results");
+// scriptHabilitado = false;
 
 // evento onload con una funcion anonima para cargar todos los elementos del ejercicio apenas cargue el archivo.
 window.onload = function() {
-    // remueve el elemento <p>Not content yet.</p>
-    let not = document.getElementById("not_content").remove()
+    if(scriptHabilitado){
 
-    div.classList = "grid grid-cols-1 md:grid-cols-3 gap-4 text-black";
+         // remueve el elemento <p>Not content yet.</p>
+        let not = document.getElementById("not_content").remove()
 
-    // se crean los elementos input y button para trabajar los datos del ejercicio.
-    let input_producto = document.createElement("input");
-    let input_iva = document.createElement("input");
-    let button_input = document.createElement("button");
-    let resultado_final = document.createElement("p");
+        div.classList = "grid grid-cols-1 md:grid-cols-3 gap-4 text-black";
 
-    // para agregar id/type/number y demas atributos, tambien existe:
-    // input_producto.setAttribute("id", "inputProd"); // Solo acepta 2 argumentos: el nombre del atributo y su valor"
-    // producto
-    input_producto.id = "inputProd";
-    input_producto.type = "number";
-    input_producto.placeholder = "Valor producto";
-    input_producto.min = 0;
-    // iva
-    input_iva.id = "inputIva";
-    input_iva.type = "number";
-    input_iva.placeholder = "Valor IVA";
-    input_iva.min = 0;
-    input_iva.max = 100;
+        // se crean los elementos input y button para trabajar los datos del ejercicio.
+        let input_producto = document.createElement("input");
+        let input_iva = document.createElement("input");
+        let button_input = document.createElement("button");
+        let resultado_final = document.createElement("p");
+    
+        // para agregar id/type/number y demas atributos, tambien existe:
+        // input_producto.setAttribute("id", "inputProd"); // Solo acepta 2 argumentos: el nombre del atributo y su valor"
+        // producto
+        input_producto.id = "inputProd";
+        input_producto.type = "number";
+        input_producto.placeholder = "Valor producto";
+        input_producto.min = 0;
+        // iva
+        input_iva.id = "inputIva";
+        input_iva.type = "number";
+        input_iva.placeholder = "Valor IVA";
+        input_iva.min = 0;
+        input_iva.max = 100;
+    
+        // button
+        button_input.id = "calcular";
+        button_input.type = "button";
+        button_input.textContent = "Calcular total";
+        button_input.classList = "bg-gray-400 hover:bg-gray-200";
+    
+        // se adjuntan (append) los elementos al contenedor
+        div.append(input_producto, input_iva, button_input);
+    
+        let producto = document.getElementById("inputProd");
+        let iva = document.getElementById("inputIva");
+        let calcularBtn = document.getElementById("calcular");
+    
+        calcularBtn.addEventListener("click", () => {
+            let valor1 = parseFloat(producto.value);
+            let valor2 = parseFloat(iva.value);
+    
+            const resultado_iva = valor1 * valor2 / 100;
+            const resultado_masIva = valor1 + resultado_iva;
+            console.log(resultado_masIva)
+    
+            resultado_final.classList = "grid col-span-1 md:col-span-3 font-bold";
+            resultado_final.innerHTML = "El valor total + el IVA es: " + resultado_masIva;
+            div.append(resultado_final);
+    
+        })
+    
+        console.log("Script loaded correctly.")
 
-    // button
-    button_input.id = "calcular";
-    button_input.type = "button";
-    button_input.textContent = "Calcular total";
-    button_input.classList = "bg-gray-400 hover:bg-gray-200";
+    }
 
-    // se adjuntan (append) los elementos al contenedor
-    div.append(input_producto, input_iva, button_input);
 
-    let producto = document.getElementById("inputProd");
-    let iva = document.getElementById("inputIva");
-    let calcularBtn = document.getElementById("calcular");
-
-    calcularBtn.addEventListener("click", () => {
-        let valor1 = parseFloat(producto.value);
-        let valor2 = parseFloat(iva.value);
-
-        const resultado_iva = valor1 * valor2 / 100;
-        const resultado_masIva = valor1 + resultado_iva;
-        console.log(resultado_masIva)
-
-        resultado_final.classList = "grid col-span-1 md:col-span-3 font-bold";
-        resultado_final.innerHTML = "El valor total + el IVA es: " + resultado_masIva;
-        div.append(resultado_final);
-
-    })
-
-    console.log("Script loaded correctly.")
 
 }
